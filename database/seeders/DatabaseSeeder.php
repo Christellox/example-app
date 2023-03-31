6898e6be87772ef1e0c29d3f0a483afe098aee92
Christian Tello <tello@ciencias.unam.mx>
2023-03-30 22:00:58 -0600

    factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
+use App\Models\Category;
+use App\Models\User;
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +17,86 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+
+
+        $user = User::factory()->create([
+            'name' => 'John Doe'
+        ]);
+
+        Post::Factory(5)->create([
+            'user_id' => $user->id
+        ]);
+        // Category::create([
+        //     'name' => 'Familia',
+        //     'slug' => 'familia'
+        // ]);
+        // Category::create([
+        //     'name' => 'Trabajo',
+        //     'slug' => 'trabajo'
+        // ]);
+        // Category::create([
+        //     'name' => 'Hobbies',
+        //     'slug' => 'hobbies'
+        // ]);
+
+        // Post::create([
+        //     'title' => 'La familia',
+        //     'user_id' => 1,
+        //     'category_id' => 1,
+        //     'slug' =>'la-familia',
+        //     'excerpt' => 'La familia es importante',
+        //     'body' => 'La familia es importante y aquí va un texto largo 
+        //     aquí va un texto largo aquí va un texto largo aquí va un texto largo 
+        //     aquí va un texto largo aquí va un texto largo aquí va un texto largo 
+        //     aquí va un texto largo aquí va un texto largo aquí va un texto largo '
+        // ]);
+
+        // Post::create([
+        //     'title' => 'La familia 2',
+        //     'category_id' => 1,
+        //     'user_id' => 1,
+        //     'slug' =>'la-familia-2',
+        //     'excerpt' => 'La familia es importante',
+        //     'body' => 'La familia es importante y aquí va un texto largo 
+        //     aquí va un texto largo aquí va un texto largo aquí va un texto largo 
+        //     aquí va un texto largo aquí va un texto largo aquí va un texto largo 
+        //     aquí va un texto largo aquí va un texto largo aquí va un texto largo '
+        // ]);
+
+        // Post::create([
+        //     'title' => 'El trabajo ',
+        //     'category_id' => 2,
+        //     'user_id' => 1,
+        //     'slug' =>'el-trabajo',
+        //     'excerpt' => 'El trabajo es importante',
+        //     'body' => 'El trabajo es importante y aquí va un texto largo 
+        //     aquí va un texto largo aquí va un texto largo aquí va un texto largo 
+        //     aquí va un texto largo aquí va un texto largo aquí va un texto largo 
+        //     aquí va un texto largo aquí va un texto largo aquí va un texto largo '
+        // ]);
+
+        // Post::create([
+        //     'title' => 'El trabajo 2',
+        //     'category_id' => 2,
+        //     'user_id' => 1,
+        //     'slug' =>'el-trabajo-2',
+        //     'excerpt' => 'El trabajo es importante',
+        //     'body' => 'El trabajo es importante y aquí va un texto largo 
+        //     aquí va un texto largo aquí va un texto largo aquí va un texto largo 
+        //     aquí va un texto largo aquí va un texto largo aquí va un texto largo 
+        //     aquí va un texto largo aquí va un texto largo aquí va un texto largo '
+        // ]);
+
+        // Post::create([
+        //     'title' => 'Mi pasatiempo',
+        //     'category_id' => 3,
+        //     'user_id' => 1,
+        //     'slug' =>'mi-pasatiempo',
+        //     'excerpt' => 'El trabajo es importante',
+        //     'body' => 'El trabajo es importante y aquí va un texto largo 
+        //     aquí va un texto largo aquí va un texto largo aquí va un texto largo 
+        //     aquí va un texto largo aquí va un texto largo aquí va un texto largo 
+        //     aquí va un texto largo aquí va un texto largo aquí va un texto largo '
+        // ]);
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,15 +5,20 @@
         
         <h1>{{ $post->title }}</h1>
 
+        <p> 
+           Por 
+            <a href="#"> {{ $post->user->name }} </a>
+            en 
+            <a href="/categories/{{ $post->category->slug }}">{{$post->category->name}} </a>
+        </p>
+
         <div>
             
 
             {!! $post->body !!}
 
         </div>
-        <p> 
-            <a href="/categories/{{ $post->category->slug }}">{{$post->category->name}} </a>
-        </p>
+
 
     </article>
 
